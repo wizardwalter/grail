cf60b89dab80c7fdc208ace06ba07da2895f1e77
wizardwalter <67981837+wizardwalter@users.noreply.github.com>
2021-01-16 22:52:20 -0500

    1st commit

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
